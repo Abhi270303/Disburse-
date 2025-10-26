c3815e40e62684896d1986f285c53269a307be3c
Abhishek Yadav <barrelabhi45@gmail.com>
2025-10-26 18:00:35 +0530

    Add client-app module

---
+++ client-app/src/components/home/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+// Removed unused imports
+import Withdraw from "../withdraw";
+
+const Home = () => {
+
+  // If authenticated, show the withdraw component
+  return <Withdraw />;
+};
+
+export default Home;
